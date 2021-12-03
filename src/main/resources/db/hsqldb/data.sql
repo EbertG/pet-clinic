11473483361c3434a404200cd6374ec1dd3f3af3
EbertG <95449942+EbertG@users.noreply.github.com>
2021-12-02 19:57:21 -0500

    Update data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -53,4 +53,4 @@ INSERT INTO pets VALUES (15, 'Flycka', '2012-07-14', 7, 9);
 INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
 INSERT INTO visits VALUES (3, 8, '2013-01-03', 'neutered');
-INSERT INTO visits VALUES (4, 7, '2013-01-04', 'spayed');
+INSERT INTO visits VALUES (4, 7, '2013-01-04', 'spayed'); 
